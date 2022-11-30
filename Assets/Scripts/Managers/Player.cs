c541371bf8a684277ad9b0407d7116ef9e5d24c5
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-11-30 15:07:25 +0900

    f scr: Managers 전역으로 불러오기

---
+++ Assets/Scripts/Managers/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        Managers mg = Managers.GetInstance();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
