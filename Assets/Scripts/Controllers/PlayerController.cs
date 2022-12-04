01ec8402a3652080684d13bcb2b63250fc07776a
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-12-04 17:53:07 +0900

    f scr: state로 Idle, Moving 애니메이션 적용

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,76 +7,83 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
 
-    bool _moveToDest = false;
     Vector3 _destPos;
     void Start()
     {
-        Managers.Input.KeyAction -= OnKeyboard;
-        Managers.Input.KeyAction += OnKeyboard;
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
     }
 
-    void Update()
+    public enum PlayerState
     {
-        if(_moveToDest)
+        Idle,
+        Moving,
+        Die,
+    }
+    PlayerState _state = PlayerState.Idle;
+
+    private void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.01f)
         {
-            Vector3 dir= _destPos - transform.position;
-            if(dir.magnitude < 0.01f)
-            {
-                _moveToDest = false;
-            }
-            else
-            {
-                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-                transform.position +=  dir.normalized * moveDist;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
-            }
+            _state = PlayerState.Idle;
         }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+        }
+
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", 1);
+        anim.Play("WAIT_RUN");
     }
 
-    void OnKeyboard()
+    private void UpdateIdle()
     {
-        if(Input.GetKey(KeyCode.W)) 
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", 0);
+        anim.Play("WAIT_RUN");
+    }
 
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+    private void UpdateDie()
+    {
 
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+    }
 
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * Time.deltaTime * _speed;
 
+    void Update()
+    {
+        switch (_state)
+        {
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Die:
+                UpdateDie();
+                break;
         }
-        _moveToDest= false;
     }
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if(evt != Define.MouseEvent.Click)
+        if(_state == PlayerState.Die)
         {
             return;
         }
+        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
         {
             _destPos = hit.point;
-            _moveToDest= true;
+            _state = PlayerState.Moving;
         }
     }
 }
