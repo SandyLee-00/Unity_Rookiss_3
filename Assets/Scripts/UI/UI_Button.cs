ef1b1d3b04929574c5766e77512a293b023feeb7
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-12-12 15:13:44 +0900

    :sparkles: : UI_Base에 Bind 하는 부분 넣고 UI_Button에서 받기

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
     enum Buttons
     {
         PointButton,
@@ -27,35 +25,12 @@ public class UI_Button : MonoBehaviour
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
 
         Get<Text>((int)Texts.ScoreText).text = "BindTest";
     }
 
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects= new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for(int i = 0; i < names.Length; i++)
-        {
-            if(typeof(T) == typeof(GameObject))
-                objects[i] =  Util.FindChild(gameObject, names[i], true);
-            else
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-
-            if (objects[i] == null)
-                Debug.Log($"Failed to bind{names[i]}");
-        }
-    }
-
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
-        return objects[idx] as T;
-    }
+    
 
     int _score = 0;
     public void OnButtonClicked()
