fca6314bdc517bc799221756dd978493aa8b2843
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-12-15 18:03:19 +0900

    :sparkles: BaseScene만들어서 각 씬 시작하면 필요한 것 불러오기

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
-        Managers.UI.ShowSceneUI<UI_Inven>();
     }
 
     public enum PlayerState
---
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginScene : BaseScene
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        SceneType = Define.Scene.Login;
+    }
+
+    public override void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+}
