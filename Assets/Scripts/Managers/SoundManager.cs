cc6df2650b784b8b058419a96515ec7988916683
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-12-30 15:02:24 +0900

    💄 Sound파일들 추가

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class SoundManager
 {
-   
+    AudioSource[] _audioSources = new AudioSource[(int)Define.Sound.MaxCount];
+
+   public void Play(Define.Sound type, string path, float pitch = 1.0f)
+    {
+        if (path.Contains("Sounds/") == false)
+            path = $"Sounds/{path}";
+
+        if(type == Define.Sound.Bgm)
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if(audioClip == null)
+            {
+                Debug.Log($"AudioClip Missing{path}");
+                return;
+            }
+        }
+        else if(type == Define.Sound.Effect)
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if (audioClip == null)
+            {
+                Debug.Log($"AudioClip Missing{path}");
+                return;
+            }
+
+            _audioSources[(int)Define.Sound.Effect];
+        }
+    }
 }
