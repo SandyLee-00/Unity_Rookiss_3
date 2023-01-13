4e208c16de9b249a4073b983bd934bd360e2781d
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-01-13 14:52:35 +0900

    :sparkles: NavMesh로 플레이어 이동

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,90 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
+  [SerializeField]
+  float _speed = 10.0f;
 
-    Vector3 _destPos;
-    void Start()
+  Vector3 _destPos;
+  void Start()
+  {
+    Managers.Input.MouseAction -= OnMouseClicked;
+    Managers.Input.MouseAction += OnMouseClicked;
+
+  }
+
+  public enum PlayerState
+  {
+    Idle,
+    Moving,
+    Die,
+  }
+  PlayerState _state = PlayerState.Idle;
+
+  private void UpdateMoving()
+  {
+    Vector3 dir = _destPos - transform.position;
+    if (dir.magnitude < 0.1f)
     {
-        Managers.Input.MouseAction -= OnMouseClicked;
-        Managers.Input.MouseAction += OnMouseClicked;
+      _state = PlayerState.Idle;
+    }
+    else
+    {
+      NavMeshAgent nma = gameObject.GetOrAddComponet<NavMeshAgent>();
+      float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+      nma.Move(dir.normalized * moveDist);
 
+      if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+      {
+        _state = PlayerState.Idle;
+        return;
+      }
+
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
     }
 
-    public enum PlayerState
+    Animator anim = GetComponent<Animator>();
+    anim.SetFloat("wait_run_ratio", 1);
+    anim.Play("WAIT_RUN");
+  }
+
+  private void UpdateIdle()
+  {
+    Animator anim = GetComponent<Animator>();
+    anim.SetFloat("wait_run_ratio", 0);
+    anim.Play("WAIT_RUN");
+  }
+
+  private void UpdateDie()
+  {
+
+  }
+
+
+  void Update()
+  {
+    switch (_state)
     {
-        Idle,
-        Moving,
-        Die,
+      case PlayerState.Idle:
+        UpdateIdle();
+        break;
+      case PlayerState.Moving:
+        UpdateMoving();
+        break;
+      case PlayerState.Die:
+        UpdateDie();
+        break;
     }
-    PlayerState _state = PlayerState.Idle;
+  }
 
-    private void UpdateMoving()
+  void OnMouseClicked(Define.MouseEvent evt)
+  {
+    if (_state == PlayerState.Die)
     {
-        Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.01f)
-        {
-            _state = PlayerState.Idle;
-        }
-        else
-        {
-            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-            transform.position += dir.normalized * moveDist;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
-        }
-
-        Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", 1);
-        anim.Play("WAIT_RUN");
-    }
-
-    private void UpdateIdle()
-    {
-        Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", 0);
-        anim.Play("WAIT_RUN");
-    }
-
-    private void UpdateDie()
-    {
-
-    }
-
-
-    void Update()
-    {
-        switch (_state)
-        {
-            case PlayerState.Idle:
-                UpdateIdle();
-                break;
-            case PlayerState.Moving:
-                UpdateMoving();
-                break;
-            case PlayerState.Die:
-                UpdateDie();
-                break;
-        }
+      return;
     }
 
-    void OnMouseClicked(Define.MouseEvent evt)
-    {
-        if(_state == PlayerState.Die)
-        {
-            return;
-        }
-        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
-        {
-            _destPos = hit.point;
-            _state = PlayerState.Moving;
-        }
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+    {
+      _destPos = hit.point;
+      _state = PlayerState.Moving;
     }
+  }
 }
