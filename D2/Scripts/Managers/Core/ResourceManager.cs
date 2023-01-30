5e31d88f813a6fb532b7299d8da7ebe9e54034be
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-01-30 16:10:58 +0900

    :scroll: D2에 넣을 코드 복사하기

---
+++ D2/Scripts/Managers/Core/ResourceManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+  public T Load<T>(string path) where T : Object
+  {
+    if (typeof(T) == typeof(GameObject))
+    {
+      string name = path;
+      int index = name.LastIndexOf('/');
+      if (index >= 0)
+        name = name.Substring(index + 1);
+
+      GameObject go = Managers.Pool.GetOriginal(name);
+      if (go != null)
+        return go as T;
+    }
+
+    return Resources.Load<T>(path);
+  }
+
+  public GameObject Instantiate(string path, Transform parent = null)
+  {
+    GameObject orginal = Load<GameObject>($"Prefabs/{path}");
+    if (orginal == null)
+    {
+      Debug.Log($"Failed  to load prefab: {path}");
+      return null;
+    }
+    if (orginal.GetComponent<Poolable>() != null)
+      return Managers.Pool.Pop(orginal, parent).gameObject;
+
+    GameObject go = Object.Instantiate(orginal, parent);
+    go.name = orginal.name;
+
+    return go;
+  }
+  public void Destroy(GameObject go)
+  {
+    if (go == null)
+      return;
+
+    Poolable poolable = go.GetComponent<Poolable>();
+    if (poolable != null)
+    {
+      Managers.Pool.Push(poolable);
+      return;
+    }
+    Object.Destroy(go);
+  }
+}
---
+++ D2/Scripts/Scenes/GameScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+  protected override void Init()
+  {
+    base.Init();
+    SceneType = Define.Scene.Game;
+
+    Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
+    gameObject.GetOrAddComponent<CursorController>();
+
+    GameObject player = Managers.Game.Spawn(Define.WorldObject.Player, "UnityChan");
+    Camera.main.gameObject.GetOrAddComponent<CameraController>().SetPlayer(player);
+    
+    GameObject go = new GameObject { name = "SpawningPool" };
+    SpawningPool pool = go.GetOrAddComponent<SpawningPool>();
+    pool.SetKeepMonsterCount(5);
+  }
+  public override void Clear()
+  {
+
+  }
+}
