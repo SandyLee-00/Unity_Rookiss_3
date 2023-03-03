75e0de1bb8ca7ef6d76996b6b7a6588509994c50
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-03-03 20:07:13 +0900

    :sparkles: EXP 표시하기

--- Assets/Scripts/Contents/PlayerStat.cs
+++ Assets/Scripts/Contents/PlayerStat.cs
@@ -4,60 +4,67 @@ using UnityEngine;
 
 public class PlayerStat : Stat
 {
-  private int _exp;
-  [SerializeField]
-  private int _gold;
+    private int _exp;
+    [SerializeField]
+    private int _maxExp;
+    private int _nextmaxExp;
+
+    private int _gold;
 
   // test 
-  public int Exp
-  {
-    get { return _exp; }
-    set
+    public int Exp
     {
-      _exp = value;
-      int level = Level;
-      while (true)
-      {
-        Data.Stat stat;
-        if (Managers.Data.StatDict.TryGetValue(level + 1, out stat) == false)
-          break;
-        if (_exp < stat.totalExp)
-          break;
-        level++;
-      }
-      if (level != Level)
-      {
-        Level = level;
-        SetStat(Level);
-      }
+        get { return _exp; }
+        set
+        {
+            _exp = value;
+            int level = Level;
+            while (true)
+            {
+                Data.Stat stat;
+                if (Managers.Data.StatDict.TryGetValue(level + 1, out stat) == false)
+                    break;
+                if (_exp < stat.totalExp)
+                    break;
+                level++;
+            }
+            if (level != Level)
+            {
+                Level = level;
+                SetStat(Level);
+            }
+        }
     }
-  }
-  public int Gold { get { return _gold; } set { _gold = value; } }
-  public int MaxExp{ get { return _maxExp; } }
+    public int Gold { get { return _gold; } set { _gold = value; } }
+    public int MaxExp{ get { return _maxExp; } }
+    public int NextMaxExp { get { return _nextmaxExp; } }
 
 
-  private void Start()
-  {
-    _level = 1;
-    _exp = 0;
-    _defense = 1;
-    _moveSpeed = 5.0f;
-    _gold = 0;
-    SetStat(_level);
-  }
+    private void Start()
+    {
+        _level = 1;
+        _exp = 0;
+        _defense = 1;
+        _moveSpeed = 5.0f;
+        _gold = 0;
+        SetStat(_level);
+    }
 
-  public void SetStat(int level)
-  {
-    Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
-    Data.Stat stat = dict[level];
+    public void SetStat(int level)
+    {
+        Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
+        Data.Stat stat = dict[level];
+        Data.Stat nextstat = dict[level+1];
 
-    _hp = stat.maxhp;
-    _maxHp = stat.maxhp;
-    _attack = stat.attack;
-    _maxExp = stat.totalExp;
-  }
-  protected override void OnDead(Stat attacker)
-  {
-    Start();
-  }
+        _hp = stat.maxhp;
+        _maxHp = stat.maxhp;
+        _attack = stat.attack;
+        _maxExp = stat.totalExp;
+        _nextmaxExp = nextstat.totalExp;
+    }
+
+    protected override void OnDead(Stat attacker)
+    {
+        _hp += 100;
+    }
 }
--- Assets/Scripts/UI/Scene/UI_Stat.cs
+++ Assets/Scripts/UI/Scene/UI_Stat.cs
@@ -5,35 +5,35 @@ using UnityEngine.UI;
 
 public class UI_Stat : UI_Scene
 {
-  public enum GameObjects
-  {
-    UI_Stat_HP_Bar,
-    UI_Stat_EXP_Bar
-  }
-  public enum Texts
-  {
-    UI_Stat_Level_Text,
-  }
-  PlayerStat _stat;
-  private GameObject Player;
+    public enum GameObjects
+    {
+        UI_Stat_HP_Bar,
+        UI_Stat_EXP_Bar
+    }
+    public enum Texts
+    {
+        UI_Stat_Level_Text,
+    }
+    PlayerStat _stat;
+    private GameObject Player;
 
-  public override void Init()
-  {
-    base.Init();
-    Bind<GameObject>(typeof(GameObjects));
-    Bind<Text>(typeof(Texts));
-    Player = Managers.Game.GetPlayer();
-    _stat = Player.GetComponent<PlayerStat>();
-  }
+    public override void Init()
+    {
+        base.Init();
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Text>(typeof(Texts));
+        Player = Managers.Game.GetPlayer();
+        _stat = Player.GetComponent<PlayerStat>();
+    }
 
-  private void Update()
-  {
-    float HPRatio = _stat.Hp / (float)_stat.MaxHp;
-    GetObject((int)GameObjects.UI_Stat_HP_Bar).GetComponent<Slider>().value = HPRatio;
-    float ExpRatio = _stat.Exp / (float)_stat.MaxExp;
-    GetObject((int)GameObjects.UI_Stat_EXP_Bar).GetComponent<Slider>().value = ExpRatio;
-    Debug.Log($"{_stat.Exp} {_stat.MaxExp}");
-  }
+    private void Update()
+    {
+        float HPRatio = _stat.Hp / (float)_stat.MaxHp;
+        GetObject((int)GameObjects.UI_Stat_HP_Bar).GetComponent<Slider>().value = HPRatio;
+        float ExpRatio = _stat.Exp / (float)_stat.NextMaxExp - (float)_stat.MaxExp ;
+        GetObject((int)GameObjects.UI_Stat_EXP_Bar).GetComponent<Slider>().value = ExpRatio;
+        Debug.Log($"{_stat.Exp} {_stat.MaxExp}");
+    }
 
 
 }
